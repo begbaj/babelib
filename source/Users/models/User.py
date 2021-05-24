ad1b6895b332b0fac9ff972c608cf622ff8ae493
benFactotum99 <S1092454@studenti.univpm.it>
2021-05-24 20:22:00 +0200

    Database manager changes and add user model

---
+++ source/Users/models/User.py
@@ -0,0 +1,53 @@
+class User:
+
+    id = 0
+    nationality_id = 0
+    state_id = 0
+    user_type_id = 0
+    username = 0
+    registration_date = '2000-01-01 00:00:00'
+    name = ''
+    surname = ''
+    gender = ''
+    birthplace = ''
+    birthdate = '2000-01-01 00:00:00'
+    city = ''
+    address = ''
+    postal_code = ''
+    distrit = ''
+    first_cellphone = ''
+    telephone = ''
+    email = ''
+    fiscal_code = ''
+    contect_mode = ''
+    privacy_agreement = 0
+
+    def __init__(self):
+        pass
+
+    def __init__(self,id, nationality_id, state_id, user_type_id, username
+                 , registration_date, name, surname, gender, birthplace
+                 , birthdate, city, address, postal_code, distrit,
+                 first_cellphone, telephone, email, fiscal_code, contect_mode, privacy_agreement):
+
+        self.id = id
+        self.nationality_id = nationality_id
+        self.state_id = state_id
+        self.user_type_id = user_type_id
+        self.username = username
+        self.registration_date = registration_date
+        self.name = name
+        self.surname = surname
+        self.gender = gender
+        self.birthplace = birthplace
+        self.birthdate = birthdate
+        self.city = city
+        self.address = address
+        self.postal_code = postal_code
+        self.distrit = distrit
+        self.first_cellphone = first_cellphone
+        self.telephone = telephone
+        self.email = email
+        self.fiscal_code = fiscal_code
+        self.contect_mode = contect_mode
+        self.privacy_agreement = privacy_agreement
--- source/database/DatabaseManager.py
+++ source/database/DatabaseManager.py
@@ -1,5 +1,8 @@
 import mariadb
 
+from source.Users.models.User import User
+
+
 class DatabaseManager:
     """
     DatabaseManager handles the connection to the database
@@ -7,12 +10,14 @@ class DatabaseManager:
     ----
     Attributes:
 
+
     ----
     Methods:
 
     """
 
-    _conn: mariadb.connect()
+
+    _conn = ""
     def __init__(self, user_, password_, host_, port_, database_):
         """
         Initialize database manager
@@ -29,12 +34,53 @@ class DatabaseManager:
             port=port_,
             database=database_
         )
+        self.cur = _conn.cursor(named_tuple=True)
 
     def query(self, query: str) -> bool:
         pass
 
-    def select(self, table:str, values:str):
-        pass
+    #def select_user
+
+    def find_user_by_id(self, id):
+
+        try:
+            self.cur.execute(f"Select * from users where id = {id}")
+        except mariadb.Error as e:
+            print(f"Error: {e}")
+
+        for row in self.cur.fetchall():
+            user = User(row.Id, row.nationality_id, row.state_id, row.user_type_id, row.username
+                 , row.registration_date, row.name, row.surname, row.gender, row.birthplace
+                 , row.birthdate, row.city, row.address, row.postal_code, row.distrit
+                 ,row.first_cellphone, row.telephone, row.email, row.fiscal_code
+                 ,row.contect_mode, row.privacy_agreement)
+
+        return user
+
+    # Print List of Contacts
+    def get_users(self):
+        """Retrieves the list of contacts from the database and prints to stdout"""
+
+        # Initialize Variables
+        users = []
+
+        # List users
+
+        try:
+            self.cur.execute(f"Select * from users")
+        except mariadb.Error as e:
+            print(f"Error: {e}")
+
+        for row in self.cur.fetchall():
+            user = User(row.Id, row.nationality_id, row.state_id, row.user_type_id, row.username
+                        , row.registration_date, row.name, row.surname, row.gender, row.birthplace
+                        , row.birthdate, row.city, row.address, row.postal_code, row.distrit
+                        , row.first_cellphone, row.telephone, row.email, row.fiscal_code
+                        , row.contect_mode, row.privacy_agreement)
+            users.append(user)
+
+        return users
+
 
     def select(self, table:str, values:str, where:str):
         pass
