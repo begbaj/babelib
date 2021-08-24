89f0400ef3b6f41811fc80947c2ff23da6056d33
Chiara <s1093786@studenti.univpm.it>
2021-08-24 19:10:53 +0200

    Changes by began

--- src/generate_fake_data.py
+++ src/generate_fake_data.py
@@ -1,5 +1,6 @@
 from src.Users.controllers.UserManager import UserManager
 from src.Utils.Tools import *
+from src.Movements.Controllers.MovementManager import MovementManager
 
 
 def generate_users(quantity: int) -> None:
@@ -14,8 +15,15 @@ def generate_items(quantity: int) -> None:
         im.add_item(generate_random_item())
 
 
+def generate_movement(quantity: int) -> None:
+    im = ItemManager()
+    um = UserManager()
+    mm = MovementManager()
+    for i in range(0, quantity):
+        mm.add(movement_generator(im, um))
+
+
 if __name__ == "__main__":
-    #generate_users(5000)
-    generate_items(5000)
-
-
+    #generate_users(100)
+    #generate_items(100)
+    generate_movement(10000)
